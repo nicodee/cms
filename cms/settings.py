3750ed63228c2a6c0c3ad011df67650c3de0b734
nicodee <nicodemus.nutsukpui@meltwater.org>
2014-02-01 19:31:29 +0000

    create gitignore file, added new apps to settings: south, sites, flatpages

--- cms/settings.py
+++ cms/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.flatpages',
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -58,7 +61,7 @@ WSGI_APPLICATION = 'cms.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'cms.db'),
     }
 }
 
